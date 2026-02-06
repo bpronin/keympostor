9eff13afa8eadc98cf10230e21a5ffe88af349c4
bpronin <boris.i.pronin@gmail.com>
2026-02-07 00:11:22 +0300

    replace simple_logger with enhanced simplelog logging setup

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -2,7 +2,7 @@
 use crate::ui::App;
 use log::LevelFilter;
 use native_windows_gui::NativeUi;
-use simple_logger::SimpleLogger;
+use std::fs::File;
 
 pub mod indicator;
 mod kb_watch;
@@ -15,13 +15,42 @@ mod util;
 mod win_watch;
 
 fn main() {
-    SimpleLogger::new()
-        .with_module_level("keympostor", LevelFilter::Trace)
-        .with_level(LevelFilter::Warn)
-        .init()
-        .expect("Failed to initialize logger.");
+    log_panics::init();
+    setup_logger().expect("Failed to initialize logger.");
 
     App::build_ui(Default::default())
         .expect("Failed to build application UI.")
         .run();
+}
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    let base_config = fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{} {:<5} {:<32} [{}] {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S%.3f"),
+                record.level(),
+                record.target(),
+                std::thread::current().name().unwrap_or("unknown"),
+                message
+            ))
+        })
+        .filter(|metadata| {
+            metadata.target().starts_with("keympostor::")
+        });
+
+    let stdout_config = fern::Dispatch::new()
+        .level(LevelFilter::Debug)
+        .chain(std::io::stdout());
+
+    let file_config = fern::Dispatch::new()
+        .level(LevelFilter::Warn)
+        .chain(File::create("keympostor.log")?);
+
+    base_config
+        .chain(stdout_config)
+        .chain(file_config)
+        .apply()?;
+
+    Ok(())
 }
